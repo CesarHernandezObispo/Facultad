0e1a98e4aad01c2fc4d7a653bf521ca1d40b5fbd
Cesar Hernandez Obispo <CesarHdzObispo@hotmail.com>
2021-04-26 15:57:21 -0500

    Add files via upload

---
+++ Base de Datos/Simulador de Facturas/Archivos proporcionados/DatosValidacion .sql
@@ -0,0 +1,18 @@
+
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('80131502','Clave Producto/Servicio','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('80131500','Clave Producto/Servicio','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('84111506','Clave Producto/Servicio','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('03','Forma de Pago','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('P01','Uso CFDI','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('PUE','Metodo de Pago','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('DLS','Metodo de Pago','NO');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('30181800','Clave Producto/Servicio','NO');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('H87','Clave Unidad','NO');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('40','Clave Unidad','NO');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('01','Forma de Pago','NO');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('E48','Clave Unidad','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('G03','Uso CFDI','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('PPD','Metodo de Pago','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('02','Forma de Pago','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('ACT','Clave Unidad','SI');
+Insert into DatosValidacionFacturas (VALOR,DESCRIPCION,ESTATUS) values ('99','Forma de Pago','SI');
